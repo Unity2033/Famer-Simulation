02ca1f7baee7426707349a205a3b1cda2468804e
mol1986@naver.com <kileab@naver.com>
2022-06-13 19:52:41 +0900

    Insert Sort

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -2,61 +2,84 @@
 
 using namespace std;
 
-// 선택 정렬
-/*
-void Select(int array[], int n)
+void Insert(int array[], int n)
 {
-	int temp = 0; 
-	int index = 0;
+	int temp, j;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i < n; i++)
 	{
-		int min = 1000;
-
-		for (int j = i; j < n; j++)
-		{
-			if (min > array[j])
-			{
-				min = array[j];
-				index = j;
-			}
-		}
-
+        // 45 <----- 45
 		temp = array[i];
 
-		array[i] = array[index];
+		//   j = 3    조건 (3 > 0 && 99 > 45) 
+		for (j = i; i > 0 && array[j - 1] > temp; j--) // j를 --하므로 j는 0번째 인덱스를 가집니다.
+		{
+			// 성립이 되었을 때
+			// [3]99 <---- [2] 99
+			array[j] = array[j - 1];
+		}
 
-		array[index] = temp;
+		// 1번째 인덱스에 처음에 94을 저장한 값을 넣어줍니다.
+		array[j] = temp;
 	}
 }
-*/
-
 
 int main()
 {
-	// 선택 정렬
+	// O(n^2) -> 거품 정렬 < 선택 정렬 < 삽입 정렬
+	// 시간 복잡도 순서와 효율성
+	
+	// 삽입 정렬
 	/*
-	int array[] = { 6,7,9,3,5 };
-	int size = sizeof(array) / sizeof(int);
+	             // 23 99 94 45 38
+	int array[] = { 99,23,94,45,38 };
+	int size = sizeof(array) / sizeof(array[0]);
 
-	Select(array, size);
+	Insert(array, size);
 
-	for (int i = 0; i < size; i++) 
+	for (int i = 0; i < size; i++)
 	{
-		std::cout << array[i] << std::endl;
+		std::cout << array[i] << " ";
 	}
 	*/
 
-	// 프로그래머스 1단계 Level (곱셉)
-    /*
-	int value_1 = 0;
-	int value_2 = 0;
+	// 거꾸로 읽는 문자열을 비교해주세요.
+	// 입력 A와 입력 B를 설정합니다.
+	
+    // A에게 <- 3개의 숫자만 입력해주세요.
+	// B에게 <- 3개의 숫자만 입력해주세요.
+
+	// 입력한 숫자를 거꾸로 저장해서 비교해야 합니다.
+	// A가 159 <- 951 
+	// B가 734 <- 437 
+
+	// A와 B의 값을 비교합니다.
+	// A가 크면 A가 입력한 숫자의 반대로된 숫자를 출력해야합니다.
+	// 만약에 B가 크면 B가 입력한 숫자의 반대로된 숫자를 출력해야합니다.
+
+	string value_1;
+	string value_2;
 
 	cin >> value_1 >> value_2;
-	              
-	std::cout << value_2 % 10 * value_1 << std::endl;
-	std::cout << value_2 / 10 % 10 * value_1 << std::endl;
-	std::cout << value_2 / 100 * value_1 << std::endl;
-	std::cout << value_1 * value_2 << std::endl;
-	*/
+
+	string temp;
+
+	for (int i = value_1.length() - 1; i >= 0; i--)
+	{
+		if (value_1[i] > value_2[i])
+		{
+			temp = value_1;
+			break;
+		}
+		else if (value_1[i] < value_2[i])
+		{
+			temp = value_2;
+			break;
+		}
+	}
+
+	for (int i = value_1.length() - 1; i >= 0; i--)
+	{
+		cout << temp[i];
+	}
 }
