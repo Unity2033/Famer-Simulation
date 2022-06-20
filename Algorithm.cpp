37d257883f6d71b2e19a9ece41cb7b973bcea09a
mol1986@naver.com <kileab@naver.com>
2022-06-20 19:48:07 +0900

    Bianry Tree

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,130 +1,63 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 
-int n;
-int vertex[10];
-std::vector<int> array[10];
+int index = 7;
 
-// 가상 함수 테이블
-
-class A
+// 하나의 노드 정보를 선언합니다.
+struct Node
 {
-public:
-	virtual void FirstFunction()
-	{
-		std::cout << "첫 번째 가상함수" << std::endl;
-	}
-
-	virtual void SecondFunction()
-	{
-		std::cout << "두 번째 가상함수" << std::endl;
-	}
-
-	void ThirdFunction()
-	{
-		std::cout << "세 번째 가상함수" << std::endl;
-	}
+	int data;
+	// 자기 자신을 가리키는 구조체 포인터 
+	Node * LeftChild;
+	Node * RightChild;
 };
 
-class B : public A
+// 전위 순회
+void Preorder(Node * pointer)
 {
-public:
-	void FirstFunction()
-	{
-		std::cout << "B의 가상함수" << std::endl;
-	}
+	 if (pointer)
+	 {
+		 std::cout << pointer->data << " ";
+		 Preorder(pointer->LeftChild);
+		 Preorder(pointer->RightChild);
+	 }
 
-};
-
-
-// 위상 정렬
-/*
-void TopologySort()
-{
-	int result[10];
-	std::queue<int> Queue;
-
-	// 진입 차수가 0인 노드를 큐에 넣어주어야 합니다.
-	for (int i = 1; i <= n; i++)
-	{
-		if (vertex[i] == 0)
-		{
-			Queue.push(i);
-		}
-	}
-
-	// 정렬이 완전히 수행되려면 정확히 n개의 노드를 방문하면 됩니다.
-	for (int i = 1; i <= n; i++)
-	{
-		// n개를 방문하기 전에 큐가 비어있으면 사이클 발생한 것으로 간주합니다.
-		// 큐가 비어있다면 함수를 반환합니다.
-		if (Queue.empty())
-		{
-			return;
-		}
-
-		int temp = Queue.front();
-		Queue.pop();
-		result[i] = temp;
-
-		for (int j = 0; j < array[temp].size(); j++)
-		{
-			int count = array[temp][j];
-
-			// 새롭게 진입차수가 0이된 정점을 큐에 삽입합니다.
-			if (--vertex[count] == 0)
-			{
-				Queue.push(count);
-			}
-		}
-	}
-
-	for (int i = 1; i <= n; i++)
-	{
-		std::cout << result[i] << " ";
-	}
+	 // 루트 노드 -> 왼쪽 하위 노드 -> 오른쪽 하위 노드
+	 // 1 2 4 5 3 6 7
+	 // 재귀 함수로 자기 자신을 호출합니다.
 }
-*/
+
+// 중위 순회
+
+
+
+// 후위 순회
+
 
 
 int main()
 { 
-	// 위상 정렬
-	/*
-	n = 7;
+	struct Node node[8];
 
-	array[1].push_back(2);
-	vertex[2]++;
+	for (int i = 1; i <= index; i++)
+	{
+		node[i].data = i;
+		node[i].LeftChild = nullptr;
+		node[i].RightChild = nullptr;
+	}
 
-	array[1].push_back(5);
-	vertex[5]++;
+	for (int i = 1; i <= index; i++)
+	{
+		if (i % 2 == 0)
+		{
+			node[i / 2].LeftChild = &node[i];
+		}
+		else
+		{
+			node[i / 2].RightChild = &node[i];
+		}
+	}
 
-	array[2].push_back(3);
-	vertex[3]++;
-
-	array[3].push_back(4);
-	vertex[4]++;
-
-	array[4].push_back(6); // <- 이 부분 간선이 잘못 배치되어 있었습니다.
-	vertex[6]++;
-
-	array[5].push_back(6);
-	vertex[6]++;
-
-	array[6].push_back(7);
-	vertex[7]++;
-
-	TopologySort();
-	*/
-
-	A * a = new A;
-	B * b = new B;
-
-	a->FirstFunction();
-	b->FirstFunction();
-	a->SecondFunction();
-	b->SecondFunction();
+	Preorder(&node[1]);
 
 	return 0;
 }
