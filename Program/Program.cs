9871ed78b8462121e23b76cbe8c6c992819193a3
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-06-12 15:45:32 +0900

    (2024.6.12) 원형 연결 리스트

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Program
+﻿
+
+namespace Program
 {
     public class DoubleLinkedList<T>
     {
-        private class Node
+        public class Node
         {
             public T data;
             public Node next;
@@ -21,7 +23,7 @@
             tail = null;
         }
 
-        void PushBack(T data)
+        public void PushBack(T data)
         {
             Node newNode = new Node();
 
@@ -44,15 +46,129 @@
                 newNode.data = data;
                 newNode.next = null;
             }
+
+            size++;
         }
 
+        public void RemoveBack()
+        {
+            if(tail == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                if(head == tail)
+                {
+                    tail = tail.next;
+                    head = tail;
+                }
+                else
+                {
+                    tail.previous.next = null;
+                    tail = tail.previous;
+                }
+
+                size--;
+            }
+        }
+
+        public void PushFront(T data)
+        {
+            if(head == null)
+            {
+                head = new Node();
+                tail = head;
+
+                head.data = data;
+                head.next = null;
+                head.previous = null;
+            }
+            else
+            {
+                Node newNode = new Node();
+
+                newNode.next = head;
+
+                head.previous = newNode;
+                head = newNode;
+
+                newNode.data = data;
+                newNode.previous = null;
+            }
+
+            size++;
+        }
+
+        public void RemoveFront()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                if(head == tail)
+                {
+                    head = head.previous;
+                    tail = head;
+                }
+                else
+                {
+                    head.next.previous = null;
+                    head = head.next;
+                }
+
+                size--;
+            }
+        }
+
+        public Node First()
+        {
+            return head;
+        }
+
+        public Node Last()
+        {
+            return tail;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Show()
+        {
+            Node currentNode = head;
+
+            while(currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+             DoubleLinkedList<int> doubleLinkedList = new DoubleLinkedList<int>();
+
+             DoubleLinkedList<int>.Node node;
+
+             doubleLinkedList.PushFront(30);
+             doubleLinkedList.PushFront(20);
+             doubleLinkedList.PushFront(10);
+             
+             doubleLinkedList.RemoveFront();
+             doubleLinkedList.RemoveFront();
+             doubleLinkedList.RemoveFront();
+
+             doubleLinkedList.Show();
+
+        
         }
     }
 }
