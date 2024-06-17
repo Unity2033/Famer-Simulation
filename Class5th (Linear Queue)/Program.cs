683f71b45d9b5f10b46fc07c34e9f10bec1633e0
Unity2033 <kileab@naver.com>
2024-06-17 15:44:55 +0900

    (2024.6.17) 원형 큐

---
+++ Class5th (Linear Queue)/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Class5th__Linear_Queue_
+{
+    public class LinearQueue<T>
+    {
+        private T error;
+        private int rear;
+        private int front;
+        private int count;
+
+        private T[] array;
+        private int arraySize;
+
+        public LinearQueue()
+        {
+            rear = 0;
+            front = 0;
+            count = 0;
+            arraySize = 5;
+
+            array = new T[arraySize];
+        }
+
+        public void Enqueue(T data)
+        {
+            if (rear < arraySize)
+            {
+                array[rear++] = data;
+
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Linear Queue is Full");
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (front == rear)
+            {
+                Console.WriteLine("Linear Queue is Empty");
+                return error;
+            }
+            else
+            {
+                T data = array[front++];
+
+                count--;
+
+                return data;
+            }
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public T Peek()
+        {
+            if (front == rear)
+            {
+                Console.WriteLine("Linear Queue is Empty");
+
+                return error;
+            }
+            else
+            {
+                return array[front];
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearQueue<int> linearQueue = new LinearQueue<int>();
+
+            linearQueue.Enqueue(10);
+            linearQueue.Enqueue(20);
+            linearQueue.Enqueue(30);
+            linearQueue.Enqueue(40);
+            linearQueue.Enqueue(50);
+
+            Console.WriteLine("linearQueue의 Peek : " + linearQueue.Peek());
+            Console.WriteLine("linearQueue의 Dequeue : " + linearQueue.Dequeue());
+            Console.WriteLine("linearQueue의 Dequeue : " + linearQueue.Dequeue());
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,95 +1,31 @@
 ﻿namespace Program
 {
-    public class Stack<T>
+    public class CircleQueue<T>
     {
-        private int top;
-    
-        private readonly int arraySize; 
-        private T error;
+        private int count;
+        private int rear;
+        private int front;
+        
+        private int arraySize;
         private T [] array;
-    
-        public Stack()
+        
+        public CircleQueue()
         {
-            top = -1;
-            arraySize = 10;
+            count = 0;
+            arraySize = 5;
+
+            front = arraySize - 1;
+            rear = arraySize - 1;
+
             array = new T[arraySize];
-        }
-
-        public void Push(T data)
-        {
-            if (top >= arraySize - 1)
-            {
-                Console.WriteLine("Stack Overflow");
-            }
-            else
-            {
-                array[++top] = data;
-            }
-        }
-
-        public T Peek()
-        {
-            return array[top];
-        }
-
-        public T Pop()
-        {
-            if (top <= -1)
-            {
-                Console.WriteLine("Stack is Empty");
-                return error;
-            }
-            else
-            {
-                return array[top--];
-            }
-        }
-
-        public bool Constains(T data)
-        {
-            for(int i = 0; i <= top; i++)
-            {
-                if (array[i].ToString() == data.ToString())
-                {
-                    return true;              
-                }
-            }
-
-            return false;
         }
     }
 
     internal class Program
     {
-        // static bool CheckBracket(string content)
-        // {
-        //     if(content.Length <= 0)
-        //     {
-        //         return false;
-        //     }
-        // 
-        //     Stack<char> stack = new Stack<char>();
-        // 
-        // }
-
         static void Main(string [] args)
         {
-            Stack<int> stack = new Stack<int>();
-
-            // stack.Push(10);
-            // stack.Push(20);
-            // stack.Push(30);
-            // stack.Push(40);
-            // stack.Push(50);
-            // 
-            // Console.WriteLine("Stack의 Constains() : " + stack.Constains(99));
-            // 
-            // Console.WriteLine("Stack의 Pop : " + stack.Pop());
-            // Console.WriteLine("Stack의 Pop : " + stack.Pop());
-            // Console.WriteLine("Stack의 Pop : " + stack.Pop());
-            // Console.WriteLine("Stack의 Pop : " + stack.Pop());
-            // Console.WriteLine("Stack의 Pop : " + stack.Pop());
-            // Console.WriteLine("Stack의 Pop : " + stack.Pop());
+           
         }
     }
 }
