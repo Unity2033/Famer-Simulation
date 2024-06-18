b88868a749230a4e98332f4fcb038ccc6a98804a
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-06-18 15:47:15 +0900

    (2024.6.18) 벡터

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,23 +1,85 @@
-﻿namespace Program
+﻿using System.ComponentModel;
+
+namespace Program
 {
-    public class CircleQueue<T>
+    public class Vector<T>
     {
-        private int count;
-        private int rear;
-        private int front;
-        
-        private int arraySize;
+        private int size;
+        private int capacity;
+
         private T [] array;
         
-        public CircleQueue()
+        public Vector()
         {
-            count = 0;
-            arraySize = 5;
+            size = 0;
+            capacity = 0;
 
-            front = arraySize - 1;
-            rear = arraySize - 1;
+            array = null;
+        }
 
-            array = new T[arraySize];
+        public void Resize(int newSize)
+        {
+            // 1. capacity에 새로운 newSize 값을 넣어줍니다.
+            capacity = newSize;
+
+            // 2. 새로운 배열을 생성합니다.
+            T [] newArray = new T[capacity];
+            
+            // 3. 기존 배열에 있는 값을 복사해서 새로운 배열에
+            //    넣어줍니다.
+            for(int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            // 4. array의 주소를 newArray의 값으로 넣어줍니다.
+            array = newArray;
+        }
+
+        public void Add(T data)
+        {
+            if(capacity <= 0)
+            {
+                Resize(1);
+            }
+            else if(size >= capacity)
+            {
+                Resize(capacity * 2);
+            }
+
+            array[size++] = data;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < size-1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[size - 1] = default;
+        }
+
+        public void Reserve(int newSize)
+        {
+            if(newSize < capacity)
+            {
+                return;
+            }
+            else
+            {
+                Resize(newSize);
+            }
+        }
+
+        public int Count()
+        {
+            return size;
+        }
+
+        public T this [int index]
+        {
+            get { return array[index]; }
         }
     }
 
@@ -25,7 +87,8 @@
     {
         static void Main(string [] args)
         {
-           
+          
+
         }
     }
 }
