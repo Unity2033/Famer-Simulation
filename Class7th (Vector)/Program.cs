f35966c522fb7ec4276c973088bc5ae5210c939b
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-06-19 15:41:53 +0900

    (2024.6.19) 문자열

---
+++ Class7th (Vector)/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Class7th__Vector_
+{
+    public class Vector<T>
+    {
+        private int size;
+        private int capacity;
+
+        private T[] array;
+
+        public Vector()
+        {
+            size = 0;
+            capacity = 0;
+
+            array = null;
+        }
+
+        public void Resize(int newSize)
+        {
+            // 1. capacity에 새로운 newSize 값을 넣어줍니다.
+            capacity = newSize;
+
+            // 2. 새로운 배열을 생성합니다.
+            T[] newArray = new T[capacity];
+
+            // 3. 기존 배열에 있는 값을 복사해서 새로운 배열에
+            //    넣어줍니다.
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            // 4. array의 주소를 newArray의 값으로 넣어줍니다.
+            array = newArray;
+        }
+
+        public void Add(T data)
+        {
+            if (capacity <= 0)
+            {
+                Resize(1);
+            }
+            else if (size >= capacity)
+            {
+                Resize(capacity * 2);
+            }
+
+            array[size++] = data;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < size - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[size - 1] = default;
+
+            size--;
+        }
+
+        public void Reserve(int newSize)
+        {
+            if (newSize < capacity)
+            {
+                return;
+            }
+            else
+            {
+                Resize(newSize);
+            }
+        }
+
+        public int Count()
+        {
+            return size;
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector<int> vector = new Vector<int>();
+
+            vector.Reserve(10);
+
+            vector.Add(10);
+            vector.Add(20);
+            vector.Add(30);
+
+            vector.RemoveAt(1);
+
+            for (int i = 0; i < vector.Count(); i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+
+        }
+    }
+}
---
+++ Class8th (String)/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Class8th__String_
+{
+    public class String
+    {
+        private char[] array;
+        private int errorCode = -99999;
+
+        public String()
+        {
+            array = null;
+        }
+
+        public void Add(char[] content)
+        {
+            array = new char[content.Length + 1];
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                array[i] = content[i];
+            }
+        }
+
+        public void Concat(char[] content)
+        {
+            char[] newArray = new char[(array.Length - 1) + content.Length + 1];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                // [A][B][C][][]
+                newArray[i] = array[i];
+            }
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                newArray[(array.Length - 1) + i] = content[i];
+            }
+
+            array = newArray;
+        }
+
+        public int IndexOf(char alphabet)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == alphabet)
+                {
+                    return i;
+                }
+            }
+
+            return errorCode;
+        }
+
+        public bool Equals(char[] content)
+        {
+            if (array.Length != content.Length + 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] != content[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
+
+        public int Size()
+        {
+            return array.Length - 1;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = new String();
+
+            name.Add(new char[] { 'A', 'B', 'C' });
+
+            name.Concat(new char[] { 'D', 'E' });
+
+            name.Show();
+
+            Console.WriteLine();
+
+            Console.WriteLine(name.IndexOf('Z'));
+            Console.WriteLine(name.Equals(new char[] { 'A', 'B', 'C', 'D', 'E', 'F' }));
+        }
+    }
+}
