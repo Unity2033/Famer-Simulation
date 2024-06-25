20f6cea49e843016ef3ac364772a3282597375f4
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-06-25 15:46:41 +0900

    (2024.6.25) 힙

---
+++ Class11th (Binary Tree)/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Class11th__Binary_Tree_
+{
+    public class Node
+    {
+        public int data;
+
+        public Node left;
+        public Node right;
+    }
+
+    internal class Program
+    {
+        static Node CreateNode(int data, Node left, Node right)
+        {
+            Node newNode = new Node();
+
+            newNode.data = data;
+
+            newNode.left = left;
+
+            newNode.right = right;
+
+            return newNode;
+        }
+
+        // 전위 순회
+        // 1. root Node를 방문합니다.
+        // 2. 왼쪽 자식 노드를 전위 순회합니다.
+        // 3. 오른쪽 자식 노드를 전위 순회합니다.
+
+        static void Preorder(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write(root.data + " ");
+                Preorder(root.left);
+                Preorder(root.right);
+            }
+        }
+
+        static void Inorder(Node root)
+        {
+            if (root != null)
+            {
+                Inorder(root.left);
+                Console.Write(root.data + " ");
+                Inorder(root.right);
+            }
+        }
+
+        static void Postorder(Node root)
+        {
+            if (root != null)
+            {
+                Postorder(root.left);
+                Postorder(root.right);
+                Console.Write(root.data + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node node7 = CreateNode(7, null, null);
+            Node node6 = CreateNode(6, null, null);
+            Node node5 = CreateNode(5, null, null);
+            Node node4 = CreateNode(4, null, null);
+
+            Node node3 = CreateNode(3, node6, node7);
+            Node node2 = CreateNode(2, node4, node5);
+
+            Node node1 = CreateNode(1, node2, node3);
+
+            //Preorder(node1);
+            //Inorder(node1);
+            //Postorder(node1);
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,67 +1,98 @@
 ﻿namespace Program
 {
-    public class Node
+    public class Heap
     {
-        public int data;
+        private int size;
+        private int arraySize;
+        private int [ ] array; 
+        
+        public Heap()
+        {
+            size = 0;
+            arraySize = 8;
 
-        public Node left;
-        public Node right;
+            array = new int[arraySize];
+        }
+
+        private void Swap(ref int x, ref int y)
+        {
+            int temporary = x;
+
+            x = y;
+
+            y = temporary;
+        }
+
+        public void Insert(int data)
+        {
+            if(size >= arraySize-1)
+            {
+                Console.WriteLine("Heap is Full");
+            }
+            else
+            {
+                array[++size] = data;
+
+                int child = size;
+                int parent = size / 2;
+
+                while(child > 1)
+                {
+                    if (array[child] > array[parent])
+                    {
+                        Swap(ref array[child], ref array[parent]);
+                    }
+
+                    child = parent;
+                    parent = child / 2;
+                }
+            }
+        }
+
+        public int Remove()
+        {
+            // 1. Heap이 비어있다면 Error 코드를 보냅니다.
+            if(size <= 0)
+            {
+                Console.WriteLine("Heap is Empty");
+                return -9999;
+            }
+
+            // 2. 임시 변수에 array[1] 값을 보관합니다.
+            int result = array[1];
+
+            // 3. size로 가리키는 배열의 값을 첫 번째 원소에 넣어줍니다.
+            array[1] = array[size];
+
+            // 4. size로 가리키는 배열의 값을 초기화합니다.
+            array[size] = 0;
+
+            // 5. size의 값을 감소시킵니다.
+            size--;
+
+        }
+
+        public void Show()
+        {
+            for(int i = 1; i <= size; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
     }
 
     internal class Program
     {
-        static Node CreateNode(int data, Node left, Node right)
-        {
-            Node newNode = new Node();
-
-            newNode.data = data;
-
-            newNode.left = left;
-
-            newNode.right = right;
-
-            return newNode;
-        }
-
-        // 전위 순회
-        // 1. root Node를 방문합니다.
-        // 2. 왼쪽 자식 노드를 전위 순회합니다.
-        // 3. 오른쪽 자식 노드를 전위 순회합니다.
-
-        static void Preorder(Node root)
-        {
-            if(root != null)
-            {
-                Console.Write(root.data + " ");
-                Preorder(root.left);
-                Preorder(root.right);
-            }
-        }
-
-        static void Inorder(Node root)
-        {
-            if(root != null)
-            {
-                Inorder(root.left);
-                Console.Write(root.data + " ");
-                Inorder(root.right);
-            }
-        }
-
         static void Main(string[] args)
         {
-            Node node7 = CreateNode(7, null, null);
-            Node node6 = CreateNode(6, null, null);
-            Node node5 = CreateNode(5, null, null);
-            Node node4 = CreateNode(4, null, null);
+            Heap heap = new Heap();
 
-            Node node3 = CreateNode(3, node6, node7);
-            Node node2 = CreateNode(2, node4, node5);
+            heap.Insert(5);  // [5]
+            heap.Insert(7);  // [7][5]
+            heap.Insert(2);  // [7][5][2]
+            heap.Insert(10); // [10][7][2][5]
 
-            Node node1 = CreateNode(1, node2, node3);
-
-            //Preorder(node1);
-            Inorder(node1);
+            heap.Show();
         }
     }
 }
