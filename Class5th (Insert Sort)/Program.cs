a6fa9f8a6518619d89c31e1c8b8390f598c6b029
Unity2033 <kileab@naver.com>
2024-07-15 15:46:21 +0900

    (2024.7.15) 삽입 정렬

---
+++ Class5th (Insert Sort)/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Class5th__Insert_Sort_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 삽입 정렬
+            // 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+            // 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+
+            int[] array = new int[] { 9, 6, 8, 1, 3 };
+
+            int key = 0;
+            int j = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                key = array[i];
+
+                for (j = i - 1; j >= 0 && array[j] > key; j--)
+                {
+                    array[j + 1] = array[j];
+                }
+
+                array[j + 1] = key;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+            }
+
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -4,42 +4,12 @@
     {
         static void Main(string[] args)
         {
-            #region 선택 정렬
-            // 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한 결과
-            // 를 교체하는 방식으로 정렬하는 알고리즘입니다.
-
-            int[] array = new int[] { 9, 6, 7, 3, 5 };
-
-            for(int i = 0; i < array.Length; i++)
-            {
-                int min = array[i];
-
-                int select = i;
-
-                for(int j = i + 1; j < array.Length; j++)
-                {
-                    if(min > array[j])
-                    {
-                        min = array[j];
-
-                        select = j;
-                    }
-                }
-
-                int temp = array[i];
-
-                array[i] = array[select];
-
-                array[select] = temp;
-            }
-
-            for(int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
+            #region 계수 정렬
+            // 데이터의 값을 직접 비교하지 않고, 단순하게
+            // 각 숫자가 몇 개 있는 지 개수를 세어 저장한 다음
+            // 정렬하는 알고리즘입니다.
 
             #endregion
-
         }
     }
 }
