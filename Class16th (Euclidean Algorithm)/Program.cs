cc30f4888331fa5f505df02a6043be4d14aa1938
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-07-24 15:45:57 +0900

    (2024.7.24) 백트래킹

---
+++ Class16th (Euclidean Algorithm)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Class16th__Euclidean_Algorithm_
+{
+    internal class Program
+    {
+        static int Euclid(int x, int y)
+        {
+            if (y == 0)
+            {
+                return x;
+            }
+            else
+            {
+                return Euclid(y, x % y);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region 유클리드 호제법
+            // 2개의 자연수 또는 정식의 최대공약수를 구하는 알고리즘으로,
+            // 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+            Console.WriteLine(Euclid(78696, 19332));
+
+            #endregion
+
+
+        }
+    }
+}
