4d8a6d3e3de6c0f96f5d10ebad3e55b652d17dc2
Unity2033 <kileab@naver.com>
2024-08-28 19:46:25 +0900

    (2024.8.28) 레이 캐스트

--- Program/Assets/Class11th (Sound)/Scripts/Dragon.cs
+++ Program/Assets/Class11th (Sound)/Scripts/Dragon.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Dragon : MonoBehaviour
 {
+    private int count = 0;
+    [SerializeField] Animator animator;
     [SerializeField] AudioClip audioClip;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void Attack()
     {
+        if(count >= 10)
+        {
+            animator.SetTrigger("Die");
+            return;
+        }
+
+        count++;
+
         SoundManager.instance.Listen(audioClip);
+    }
+
+    public void Release()
+    {
+        Destroy(gameObject);
     }
 }
