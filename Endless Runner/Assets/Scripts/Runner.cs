ac63826b6e712215ae0bee6696e987a153a439ce
Unity2033 <kileab@naver.com>
2024-09-11 19:46:29 +0900

    (2024.9.11) 캐릭터 이동 시스템

--- Endless Runner/Assets/Scripts/Runner.cs
+++ Endless Runner/Assets/Scripts/Runner.cs
@@ -12,14 +12,57 @@ public enum RoadLine
 public class Runner : MonoBehaviour
 {
     [SerializeField] RoadLine roadLine;
+    [SerializeField] Rigidbody rigidBody;
+
+    [SerializeField] float speed = 25.0f;
+    [SerializeField] float positionX = 2.5f;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     void Start()
     {
-        
+        roadLine = RoadLine.MIDDLE;
+    }
+
+    private void OnkeyUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(roadLine != RoadLine.LEFT)
+            {
+                roadLine--;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(roadLine != RoadLine.RIGHT)
+            {
+                roadLine++;
+            }
+        }
     }
 
     void Update()
     {
-        
+        OnkeyUpdate();
+    }
+
+    private void Move()
+    {
+        rigidBody.position = Vector3.Lerp
+        (
+            rigidBody.position,
+            new Vector3((int)roadLine * positionX, 0, 0),
+            speed * Time.fixedDeltaTime
+        );
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 }
