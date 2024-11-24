ad3940518930459012ad2998bce111ac69dc5b7c
KathGaan <futor2003@gmail.com>
2024-11-24 22:33:15 +0900

    (2024.11.24 / 한건희) PlayfabManager 제작
    PhotonManager를 만들었으나, 무엇을 만들어야 하는지 모르겠습니다.
    강사님 파워포인트를 보았지만, PhotonManager가 보이지 않았습니다.
    
    로그인 시스템은 정상 작동을 확인했습니다.

---
+++ Famer Simulation/Assets/Scripts/PhotonScripts/PlayfabManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using PlayFab.ClientModels;
+using PlayFab;
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_InputField emailInput;
+    [SerializeField] TMP_InputField passwordInput;
+    [SerializeField] TextMeshProUGUI AlarmText;
+
+    public void Success(LoginResult result)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Photon Lobby");
+    }
+
+    public void Success(RegisterPlayFabUserResult result)
+    {
+        AlarmText.text = "(Dummy) Register Success!";
+    }
+
+    public void SignUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+
+    public void LogIn()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request, Success, Failure
+        );
+    }
+
+    public void Failure(PlayFabError error)
+    {
+        AlarmText.text = error.ErrorMessage;
+    }
+}
